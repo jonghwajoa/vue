56be3a4fb4ca8c6a0ce47f4c15329ab9e7839bbd
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-03-02 22:22:56 +0900

    feat: 투두리스트 완성

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,14 +1,68 @@
+/* eslint-disable vue/require-v-for-key */
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-4">Todo 어플리케이션</h1>
+        <input type="text" class="form-control" v-model="userInput" @keyup.enter="addNewTodo" />
+
+        <div class="list-group mb-4">
+          <template v-for="todo in activeTodoList">
+            <Todo
+              :label="todo.label"
+              @componentClick="toggleTodoState(todo)"
+              :key="todo.label"
+            ></Todo>
+          </template>
+        </div>
+
+        <div class="text-right">
+          <button class="btn btn-sm" @click="changeCurrentState('active')">할일</button>
+          <button class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
+          <button class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Todo from './components/Todo';
+
 export default {
-  name: "App",
-  components: {}
+  name: 'App',
+  data() {
+    return {
+      userInput: '',
+      todoList: [],
+      currentState: 'active',
+    };
+  },
+  computed: {
+    activeTodoList() {
+      return this.todoList.filter(
+        todo => this.currentState === 'all' || todo.state === this.currentState,
+      );
+    },
+  },
+  methods: {
+    changeCurrentState(state) {
+      this.currentState = state;
+    },
+    addNewTodo() {
+      this.todoList.push({
+        label: this.userInput,
+        state: 'active',
+      });
+      this.userInput = '';
+    },
+    toggleTodoState(todo) {
+      todo.state = todo.state === 'active' ? 'done' : 'active';
+    },
+  },
+  components: {
+    Todo,
+  },
 };
 </script>
 
